bbe9dde2952cf7371c869ca0367b94c144256418
alkanyuce93 <alkan.yuce.93@gmail.com>
2023-12-14 13:30:12 +0300

    Remove unused components and styles

--- src/components/services/index.jsx
+++ src/components/services/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
-import Box from "../box";
+import { Row, Col, Card } from "react-bootstrap";
 import "../container/container.css";
 import CustomButton from "../customButton";
 
 const Services = () => {
   return (
-    <Row className="margin-top-50">
-      <Col xl={4} md={12}>
+    <Row className="mt-5 paddin-top-300">
+      <Col xxl={4} md={12}>
         <div>
           <h1 className="header-2-text">
             Anything you need, we’ve got you covered
@@ -15,27 +14,44 @@ const Services = () => {
           <CustomButton type="xsmall" text={"Get in Touch"} />
         </div>
       </Col>
-      <Col xl={4} md={6} sm={12}>
-        <div>
-          <Box
-            image="./bag.png"
-            imageWidth={60}
-            imageHeight={60}
-            header="Web & Graphic Design"
-            content="Vestibulum ac diam sit amet quam vehicula elementum amet est on dui. Nulla porttitor accumsan tincidunt."
-          />
-        </div>
+      <Col className="card-padding" xxl={4} sm={6} xs={12}>
+        <Card className="rounded-0 shadow ">
+          <Card.Body className="p-5">
+            <Card.Img
+              style={{ height: 60 }}
+              className="mb-5"
+              variant="top-left"
+              src="./bag.png"
+            />
+            <Card.Title style={{ color: "#314584" }} className="mb-3">
+              Web & Graphic Design
+            </Card.Title>
+            <Card.Text style={{ color: "#314584" }}>
+              Vestibulum ac diam sit amet quam vehicula elementum amet est on
+              dui. Nulla porttitor accumsan tincidunt.
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </Col>
-      <Col xl={4} md={6} sm={12}>
-        <div c>
-          <Box
-            image="./screen.png"
-            imageWidth={70}
-            imageHeight={50}
-            header="Web & App Development"
-            content="Vestibulum ac diam sit amet quam vehicula elementum amet est on dui. Nulla porttitor accumsan tincidunt."
-          />
-        </div>
+      <Col className="card-padding" xxl={4} sm={6} xs={12}>
+        <Card className="rounded-0 shadow ">
+          <Card.Body className="p-5">
+            <Card.Img
+              style={{ height: 60 }}
+              className="mb-5"
+              variant="top-left"
+              src="./screen.png"
+            />
+            <Card.Title style={{ color: "#314584" }} className="mb-3">
+              Web & Graphic Design
+            </Card.Title>
+
+            <Card.Text style={{ color: "#314584" }}>
+              Vestibulum ac diam sit amet quam vehicula elementum amet est on
+              dui. Nulla porttitor accumsan tincidunt.
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </Col>
     </Row>
   );
